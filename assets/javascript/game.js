83e84b9ce07ec19665846c2d3027ffeb04b11035
Karin Rimstidt <Karin Rimstidt>
2017-03-19 18:18:05 -0500

    tried to fix this

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Psychic Game</title>
-</head>
-<body>
-<script type="text/javascript">
+// <!DOCTYPE html>
+// <html>
+// <head>
+// 	<title>Psychic Game</title>
+// </head>
+// <body>
+// <script type="text/javascript">
 
 //define variables for wins, losses, and guesses left and set starting values
-var wins=0
+var wins=0;
 //how do i keep wins from resetting to zero once incremented?
-var losses=0
-var guessesLeft=9
+var losses=0;
+var guessesLeft=9;
 
 //define acceptable options
-var options=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var options=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 //function to capture user guess key
 document.onkeyup=function(event){
 	//determines which key was chosen and converts to lowercase and stores it
 	var userGuess=String.fromCharCode(event.keyCode).toLowerCase();
 	//prints selection to screen
-	alert(userGuess);
+	prompt(userGuess);
 	//computer randomly picks a letter
 	var computerGuess=options[Math.floor(Math.random()*options.length)];
 	console.log(computerGuess);
@@ -29,19 +29,22 @@ document.onkeyup=function(event){
 	//if the same
 	//	increment Wins
 	//  display to screen
-	if (userGuess==computerGuess){
-		(alert("Wow, you ARE a psychic...you guessed correctly!")
-		var wins= wins++
-		alert("Wins= " + wins);
+	function winMessage(){
+		if (userGuess==computerGuess){
+		prompt("Wow, you ARE a psychic...you guessed correctly!");
+		wins++;
+		prompt("Wins= " + wins);
 	}
 
 	//if not the same
 		//	decrement Guesses Left
-	else{(var guessesLeft=guessesLeft--)
+		else{
+			(guessesLeft--)
 			//	print letters guessed beside words: Your Guesses so far...cannot find how to do this, 
 			//  though i know we did it in class
 
-			alert("You incorrectly guessed " userGuess ". You have " guessesLeft "guesses left.")
+			prompt("You incorrectly guessed " userGuess ". You have " guessesLeft "guesses left".);
+
 			//count Guesses Leftcount guesseLeft;
 			// When Guesses Left = 0
 				//	increment losses
@@ -50,39 +53,25 @@ document.onkeyup=function(event){
 				if (guessesLeft=0){
 				guessesLeft=9;
 				losses++;
-				alert:"Try again."
+				prompt("Try again.");
 				}
 		}
 
-	
-
-
-
-
-
-
-
-
-
+	}
 
 
 //	print to screen: Try Again
 
 
-
-
-
-}
-
-</script>
-</body>
-</html><!DOCTYPE html>
-<html>
-<head>
-	<title>Psychic Game</title>
-</head>
-<body>
-<script type="text/javascript">
+// </script>
+// </body>
+// </html><!DOCTYPE html>
+// <html>
+// <head>
+// 	<title>Psychic Game</title>
+// </head>
+// <body>
+// <script type="text/javascript">
 
 
 
@@ -92,6 +81,6 @@ document.onkeyup=function(event){
 
 
 
-</script>
-</body>
-</html>
+// </script>
+// </body>
+// </html>
